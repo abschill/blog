7ce14ff05be7f7128c35a1ef0d8e605ca02c0328
abschill <ajay198810@gmail.com>
2022-05-11 16:18:51 -0600

    fix undefined mode for prod

--- src/api/loaders/index.js
+++ src/api/loaders/index.js
@@ -1,5 +1,6 @@
-const baseLoaderConfig = {
+const baseLoaderOptions = {
 	pathRoot: 'views',
+	templates: 'pages',
 	partials: 'layout',
 	partialInput: {
         meta_title: `abschill's blog`,
@@ -9,14 +10,16 @@ const baseLoaderConfig = {
     }
 }
 
-const baseLoaderOptions = {
-	...baseLoaderConfig,
-	templates: 'pages'
-}
-
 const mdLoaderOptions = {
-	...baseLoaderConfig,
-	templates: '.content'
+	pathRoot: 'views',
+	templates: '.content',
+	partials: 'layout',
+	partialInput: {
+        meta_title: `abschill's blog`,
+        meta_desc: 'tutorials for full stack software engineering and some rants',
+        icon: 'https://d24hicsohgfvzu.cloudfront.net/assets/logo144.png',
+        og_img: 'https://d24hicsohgfvzu.cloudfront.net/assets/logo512.png',
+    }
 }
 
 module.exports = {baseLoaderOptions, mdLoaderOptions};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const createAPI = require('./api');
 const http = require('http');
-module.exports = http.createServer(createAPI(process.argv[2] ?? null));
+let args = 'prod';
+if(process.argv.length > 2) {
+	args = process.argv[2];
+}
+module.exports = http.createServer(createAPI(args));
