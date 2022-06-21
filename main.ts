daf895c4d82df152ed122a6d29092ecdad132a7c
abschill <ajay198810@gmail.com>
2022-06-21 00:10:07 -0600

    update: avatar

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import blog from "https://raw.githubusercontent.com/abschill/deno_blog/main/blog.tsx";
 
 blog({
+	avatar: 'https://avatars.githubusercontent.com/u/56945687?s=400&u=8cd0939e45a4df312e55bb83307b8ab3f4bc7407&v=4',
+	avatarClass: 'full',
 	title: "abschill's blog",
 	description: 'welcome',
 	author: 'abschill',
